bf3724f6617dcbc9a956fd765784bd62ef0ee57e
S4pphiree <s4pph1r3sam@gmail.com>
2022-11-03 21:56:53 +0100

    Aggiungere i file di progetto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace FlexLayout;
+
+public partial class MainPage : ContentPage
+{
+    Vector serial = new Vector();
+    Vector batteryLife = new Vector();
+    Vector price = new Vector();
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void btnEnter_Clicked(object sender, EventArgs e)
+	{
+		serial.Insert(Int32.Parse(txtSerial.Text));
+        batteryLife.Insert(Int32.Parse(txtBatteryLife.Text));
+        price.Insert(Int32.Parse(txtPrice.Text));
+    }
+}
+
---
+++ Vector.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexLayout
+{
+    internal class Vector
+    {
+        int[] v = new int[100];
+        int n;
+
+        public Vector()
+        {
+            n = 0;
+        }
+
+        public void Insert(int value)
+        {
+            v[n] = value;
+            n++;
+        }
+
+        public int MaxPos()
+        {
+            int p = 0, max = v[0];
+
+            for (int i = 1; i < n; i++)
+                if (v[i] > max)
+                {
+                    max = v[i];
+                    p = i;
+                }
+
+            return p;
+        }
+
+        public float Average()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+                sum = sum + v[i];
+
+            sum = sum / n;
+
+            return sum;
+        }
+
+        public int DeleteElement(int pos)
+        {
+            if (pos < n)
+            {
+                for (int i = pos; i < n; i++)
+                    if (i != n - 1)
+                        v[i] = v[i + 1];
+                    else
+                        Array.Resize<int>(ref v, n);
+                n--;
+                return 0;
+            }
+            else
+                return -1;
+        }
+
+        public string VecToString()
+        {
+            string str = "";
+            for (int i = 0; i < n; i++)
+                str = str + v[i] + "\n";
+            return str;
+        }
+
+        public string ToStringPos(int pos)
+        {
+            string str;
+            if (pos < n)
+                return str = v[pos].ToString();
+            else
+                return str = $"Posizione {pos} inesistente";
+        }
+    }
+}
